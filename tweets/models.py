062fdb81dcac547db173a97262426c1aff9fed10
haru <godjaewook@naver.com>
2024-04-22 23:42:09 +0900

    ORM, related_name

--- tweets/models.py
+++ tweets/models.py
@@ -5,15 +5,18 @@ from common.models import CommonModel
 
 class Tweet(CommonModel):
     payload = models.CharField(max_length=180)
-    users = models.ForeignKey('users.User', on_delete=models.CASCADE)    
+    users = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='tweets',)    
 
     def __str__(self):
         return self.payload
     
+    def Like_Count(self):
+        return self.likes.count()
+    
 
 class Like(CommonModel):
-    tweet = models.ForeignKey('tweets.Tweet', on_delete=models.CASCADE)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    tweet = models.ForeignKey('tweets.Tweet', on_delete=models.CASCADE, related_name='likes',)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='likes',)
 
     def __str__(self):
         return f'{self.user} likes {self.tweet}'
