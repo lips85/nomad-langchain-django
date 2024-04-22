4ba5e0a6801e33053de1f3f1c8e083ae199742be
haru <godjaewook@naver.com>
2024-04-22 23:45:09 +0900

    Revert "ORM, related_name"
    This reverts commit 062fdb81dcac547db173a97262426c1aff9fed10.

--- tweets/admin.py
+++ tweets/admin.py
@@ -4,7 +4,7 @@ from .models import Tweet, Like
 # Register your models here.
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    list_display = ('payload', 'users', "Like_Count",'created_at', 'updated_at')
+    list_display = ('payload', 'users', 'created_at', 'updated_at')
     search_fields = ('payload', 'users__name')
     list_filter = ('created_at', 'updated_at')
     date_hierarchy = 'created_at'
--- tweets/models.py
+++ tweets/models.py
@@ -5,18 +5,15 @@ from common.models import CommonModel
 
 class Tweet(CommonModel):
     payload = models.CharField(max_length=180)
-    users = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='tweets',)    
+    users = models.ForeignKey('users.User', on_delete=models.CASCADE)    
 
     def __str__(self):
         return self.payload
     
-    def Like_Count(self):
-        return self.likes.count()
-    
 
 class Like(CommonModel):
-    tweet = models.ForeignKey('tweets.Tweet', on_delete=models.CASCADE, related_name='likes',)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='likes',)
+    tweet = models.ForeignKey('tweets.Tweet', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user} likes {self.tweet}'
