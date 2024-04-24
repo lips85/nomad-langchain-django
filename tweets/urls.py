0e9ea65229b4c5fdc2109abf91fd3fe190622341
haru <godjaewook@naver.com>
2024-04-24 23:23:26 +0900

    Urls, Views and Template-test

---
+++ tweets/urls.py
@@ -0,0 +1,9 @@
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.see_all_tweets),
+    path("<int:tweet_pk>/", views.see_one_room),
+    ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Tweet
 
 # Create your views here.
+def see_all_tweets(request):
+    tweets = Tweet.objects.all()
+    return render(request, "all_tweets.html", {"tweets": tweets},)
+
+
+def see_one_room(request, tweet_pk):
+    try:
+        tweet = Tweet.objects.get(pk=tweet_pk)
+        return render(
+            request, 
+            "tweet_detail.html", 
+            {
+                "tweet": tweet,
+            },
+        )
+    except Tweet.DoesNotExist:
+        return render(
+            request, 
+            "tweet_detail.html",
+            {
+                "not_found": True,
+            },
+        )
