81481aa2190dd9c232c65efe7019f5e3a887e1d5
haru <godjaewook@naver.com>
2024-05-05 21:05:42 +0900

    마지막 융향 필요

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.UserViewset.as_view({"get": "list","post": "create",}),),
-    path("<int:pk>/tweets/",
-        views.UserTweetViewSet.as_view({'get': 'list',},),
-    )
+    path(
+        "",
+        views.UserViewset.as_view(
+            {
+                "get": "list",
+                "post": "create",
+            }
+        ),
+    ),
+    path(
+        "<int:pk>/tweets/",
+        views.UserTweetViewSet.as_view(
+            {
+                "get": "list",
+            },
+        ),
+    ),
 ]
